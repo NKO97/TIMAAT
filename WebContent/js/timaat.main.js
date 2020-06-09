135396c963920427dee134107d9453539e01d541
Mirko Scherf <mscherf@uni-mainz.de>
2020-06-09 14:03:29 +0200

    chore: release version 0.4.0

--- WebContent/js/timaat.main.js
+++ WebContent/js/timaat.main.js
@@ -10,7 +10,7 @@
 	// var yyyy = today.getFullYear();	
 	// today = yyyy + '-' + mm + '-' + dd;
 
-	var version = 'v0.3.2-dev-trunk (2020-06-05)';
+	var version = 'v0.4.0-release (2020-06-09)';
 
 	document.title = 'TIMAAT - Client '+version;
 	$('#timaat-version-info').text(version);
