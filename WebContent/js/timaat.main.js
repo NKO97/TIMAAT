0ddbcc135939139b8b9667e0625ffda02b9c0f24
Mirko Scherf <mscherf@uni-mainz.de>
2021-07-26 13:44:56 +0200

    fix: DB update script

--- WebContent/js/timaat.main.js
+++ WebContent/js/timaat.main.js
@@ -4,7 +4,7 @@
 	(factory((global.TIMAAT = {})));
 }(this, (function (exports) { 'use strict';
 
-	var version = 'v0.12.1-dev (2021-07-23)';
+	var version = 'v0.12.1-dev (2021-07-26)';
 
 	document.title = 'TIMAAT - Client '+version;
 	$('#timaat-version-info').text(version);
