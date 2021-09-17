4690c383feb74b1b03c0a7d68b633da4b1e08c81
Mirko Scherf <mscherf@uni-mainz.de>
2021-09-17 10:23:29 +0200

    refactor: TIMAATPub.video -> TIMAATPub.medium

--- WebContent/js/timaat.main.js
+++ WebContent/js/timaat.main.js
@@ -4,7 +4,7 @@
 	(factory((global.TIMAAT = {})));
 }(this, (function (exports) { 'use strict';
 
-	var version = 'v0.13.5-dev (2021-09-16)';
+	var version = 'v0.13.5a-dev (2021-09-17)';
 
 	document.title = 'TIMAAT - Client '+version;
 	$('#timaat-version-info').text(version);
