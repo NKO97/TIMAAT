9bd6a98b56c7da732c7691dc3c3ab11ee83935e4
Mirko Scherf <mscherf@uni-mainz.de>
2022-04-04 11:28:24 +0200

    chore: release version 0.15.2

--- src/resources/db_updates/fipop-update_script_2022-04-04.sql
+++ src/resources/db_updates/fipop-update_script_2022-04-04.sql
@@ -1,3 +1,6 @@
+ALTER TABLE `FIPOP`.`tag` CHANGE `name` `name` VARCHAR(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL;
+ALTER TABLE `FIPOP`.`category_set` CHANGE `name` `name` VARCHAR(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL;
+
 -- -----------------------------------------------------
 -- Table `FIPOP`.`camera_depth_of_focus`
 -- -----------------------------------------------------
